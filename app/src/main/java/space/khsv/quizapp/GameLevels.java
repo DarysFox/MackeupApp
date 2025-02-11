b80686d6e14eb09db44ba4538aec17e907fd862b
DarysFox <darya.khsv@gmail.com>
2025-02-11 16:51:17 +0300

    commit3 - level1

--- app/src/main/java/space/khsv/quizapp/GameLevels.java
+++ app/src/main/java/space/khsv/quizapp/GameLevels.java
@@ -1,9 +1,13 @@
 package space.khsv.quizapp;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,11 +24,30 @@ public class GameLevels extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.gamelevels);
         Window w = getWindow();
-        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){}
+            }
+        });
     }
-}
+            public void goBackToMainActivity(View view) {
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+            }
+
+        }
+
--- app/src/main/java/space/khsv/quizapp/Level1.java
+++ app/src/main/java/space/khsv/quizapp/Level1.java
@@ -1,14 +1,8 @@
 package space.khsv.quizapp;
 
-import static android.widget.Toast.makeText;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,25 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Level1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.univerasal);
+
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-            public void goToGameLevels(View view) {
-                    Intent intent = new Intent(this, GameLevels.class);
-                    startActivity(intent);
-                    finish();
-        };
-    }
-
+}
