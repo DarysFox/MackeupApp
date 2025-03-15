33cfc44e172013923c5e8b7f6b31bd8c8c85ef3a
DarysFox <darya.khsv@gmail.com>
2025-03-15 13:11:57 +0300

    commit7 - right wrong

--- app/src/main/java/space/khsv/quizapp/Array.java
+++ app/src/main/java/space/khsv/quizapp/Array.java
@@ -2,21 +2,21 @@ package space.khsv.quizapp;
 
 public class Array {
     final int[] images1 = {
-            R.drawable.make1,
-            R.drawable.make2,
-            R.drawable.make3,
-            R.drawable.make4,
-            R.drawable.make5,
-            R.drawable.make6,
-            R.drawable.make7,
-            R.drawable.make8,
-            R.drawable.make9,
-            R.drawable.make10,
-            R.drawable.make11,
-            R.drawable.make12,
+            R.drawable.make14,
             R.drawable.make13,
-            R.drawable.make14
+            R.drawable.make12,
+            R.drawable.make11,
+            R.drawable.make10,
+            R.drawable.make9,
+            R.drawable.make8,
+            R.drawable.make7,
+            R.drawable.make6,
+            R.drawable.make5,
+            R.drawable.make4,
+            R.drawable.make3,
+            R.drawable.make2,
+            R.drawable.make1
     };
 
-    final int[] power1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+
 }
--- app/src/main/java/space/khsv/quizapp/Level1.java
+++ app/src/main/java/space/khsv/quizapp/Level1.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -26,6 +30,8 @@ public class Level1 extends AppCompatActivity {
     Array array = new Array();
     Random random = new Random();
 
+    public int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -33,6 +39,9 @@ public class Level1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
         setContentView(R.layout.univerasal);
+
+        final ImageView img_left = (ImageView) findViewById(R.id.img_left);
+        final ImageView img_right = (ImageView) findViewById(R.id.img_right);
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
@@ -46,13 +55,168 @@ public class Level1 extends AppCompatActivity {
             }
         });
 
+        final int[] progress = {
+                R.id.point1, R.id.point2, R.id.point3, R.id.point4, R.id.point5, R.id.point6, R.id.point7,
+                R.id.point8, R.id.point9, R.id.point10, R.id.point11, R.id.point12, R.id.point13, R.id.point14,
+                R.id.point15, R.id.point16
+        };
+
+        final Animation a = AnimationUtils.loadAnimation(Level1.this, R.anim.alpha);
+
         numLeft = random.nextInt(13);
+        img_left.setImageResource(array.images1[numLeft]);
+
+        numRight = random.nextInt(13);
+//        img_right.setImageResource(array.images1[numRight]);
+
+        while (numLeft == numRight) {
+            numRight = random.nextInt(13);
+        }
+
+        img_right.setImageResource(array.images1[numRight]);
+
+        img_left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    img_right.setEnabled(false);
+                    if (numLeft > numRight) {
+                        img_left.setImageResource(R.drawable.ri);
+                    } else {
+                        img_left.setImageResource(R.drawable.wr);
+                    }
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+
+                    if (numLeft > numRight) {
+                        if (count < 16) {
+                            count = count + 1;
+                        }
+                        for (int i = 0; i < 16; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+
+                        for (int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_correct);
+                        }
+
+                    } else {
+
+                        if (count > 0) {
+                            if (count == 1) {
+                                count = 0;
+                            } else {
+                                count = count - 2;
+                            }
+                        }
+                        for (int i = 0; i < 15; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+
+                        for (int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_correct);
+                        }
+                    }
+                    if (count == 16) {
+
+                    } else {
+                        numLeft = random.nextInt(13);
+                        img_left.setImageResource(array.images1[numLeft]);
+                        img_left.startAnimation(a);
+                        numRight = random.nextInt(13);
+
+                        while (numLeft == numRight) {
+                            numRight = random.nextInt(13);
+                        }
+
+                        img_right.setImageResource(array.images1[numRight]);
+                        img_right.startAnimation(a);
+                        img_right.setEnabled(true);
+                    }
+                }
+                return true;
+            }
+        });
 
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        img_right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    img_left.setEnabled(false);
+                    if (numRight > numLeft) {
+                        img_right.setImageResource(R.drawable.ri);
+                    } else {
+                        img_right.setImageResource(R.drawable.wr);
+                    }
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+
+                    if (numLeft < numRight) {
+                        if (count < 16) {
+                            count = count + 1;
+                        }
+                        for (int i = 0; i < 16; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+
+                        for (int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_correct);
+                        }
+
+                    } else {
+
+                        if (count > 0) {
+                            if (count == 1) {
+                                count = 0;
+                            } else {
+                                count = count - 2;
+                            }
+                        }
+                        for (int i = 0; i < 15; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+
+                        for (int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_correct);
+                        }
+                    }
+                    if (count == 16) {
+
+                    } else {
+                        numLeft = random.nextInt(13);
+                        img_left.setImageResource(array.images1[numLeft]);
+                        img_left.startAnimation(a);
+                        numRight = random.nextInt(13);
+
+                        while (numLeft == numRight) {
+                            numRight = random.nextInt(13);
+                        }
+
+                        img_right.setImageResource(array.images1[numRight]);
+                        img_right.startAnimation(a);
+                        img_left.setEnabled(true);
+                    }
+                }
+                return true;
+            }
         });
     }
 }
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//}
